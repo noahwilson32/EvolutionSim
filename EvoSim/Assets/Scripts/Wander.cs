ad5d206be2e8738f504ff774fae43c31251ef4b8
NoahWilson <noahwilsonmailbox32@gmail.com>
2021-08-26 10:06:11 -0400

    Added Avatar

---
+++ EvoSim/Assets/Scripts/Wander.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+    public GameObject avatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveTo()
+    {
+        Vector3 currentPos = new Vector3Int((int)avatar.transform.position.x, (int)avatar.transform.position.y, (int)avatar.transform.position.z);
+
+    }
+}
